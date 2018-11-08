6c42c658135d746c5b0664758770d8702f93f2d6
carmenhorendum <carmenhorendum@gmail.com>
2018-11-09 01:26:17 +0300

    Update TaskFiveMain.java

--- Lecture1/Task5/packFive/TaskFiveMain.java
+++ Lecture1/Task5/packFive/TaskFiveMain.java
@@ -6,61 +6,60 @@ import java.util.Set;
 
 public class TaskFiveMain {
 	public static void main(String[] args) {
-		String answer = "Дзяніс Голін";
+		String answer = "ДЗЯНІС ГОЛІН";
 		String mask = "****** *****";
-		char[] answerArray = answer.toLowerCase().toCharArray();
+		char[] answerArray = answer.toUpperCase().toCharArray();
 		StringBuffer guessed = new StringBuffer(mask);
 		Set picked = new HashSet();
 		while (!mask.equals(answer)) {
-			System.out.println("Make your guess. You can enter a letter or a word:");
+			System.out.println();
+			System.out.println(String.format("Make your guess: %s. You can enter a letter or a word.",mask));
 			Scanner input = new Scanner(System.in);
-			String guess = input.nextLine().toLowerCase().trim();
+			String guess = input.nextLine().toUpperCase();
 			int caseId = 0;
 			if (picked.contains(guess)) {
 				caseId = 3;
 			} else {
-//				if (guess.length()==1) {
+				if (guess.length()==1) {
 					for (int i = 0; i < answerArray.length; i++) {
-						System.out.println(answerArray[i]);
-						System.out.println(guess.charAt(0));
-						System.out.println();
 						if (answerArray[i]==guess.charAt(0)) {
 							caseId = 1;
 							guessed.replace(i,i+1,answer.substring(i,i+1));
 							mask = guessed.toString();
-						} else {
+						} else if (mask.equals(answer)) {
 							caseId = 2;
 						}
 					}
-//				} else {
-//					System.out.println(guess.length());
-//				}
+				} else {
+					if (answer.toUpperCase().contains(guess)) {
+						caseId = 1;
+						int startIndex = answer.toUpperCase().indexOf(guess);
+						guessed.replace(startIndex,startIndex+guess.length(),
+							answer.substring(startIndex,startIndex+guess.length()));
+						mask = guessed.toString();
+					} else if (mask.equals(answer)) {
+						caseId = 2;
+					}
+				}
 			}
-			System.out.println(caseId);
 			switch (caseId) {
 				case 1:
 					picked.add(guess);
-					System.out.println(String.format("Success! %s",guessed));
+					System.out.println(String.format("Good guess: %s belongs to %s!",guess,guessed));
 					break;
 				case 2:
-					picked.add(guess);
-					System.out.println(String.format("Wrong guess! %s doesn't belong to %s.",guess,guessed));
-					break;
-				case 3:
-					picked.add(guess);
-					System.out.println(String.format("You've already picked letter %s! Try another one.",guess));
-					break;
-				default:
-					mask = guessed.toString();
 					System.out.println();
 					System.out.println(String.format("You win! Correct answer is: %s",answer));
 					break;
+				case 3:
+					picked.add(guess);
+					System.out.println(String.format("You've already picked letter %s! Try something different.",guess));
+					break;
+				default:
+					picked.add(guess);
+					System.out.println(String.format("Bad guess: %s doesn't belong to %s!",guess,guessed));
+					break;
 			}
-//			System.out.println(picked);
-//			System.out.println(picked.contains(guess));
-//			mask = guessed.toString();
-//			System.out.println();
-//			System.out.println(String.format("You win! Correct answer is: %s",answer));
 		}
 	}
 }
