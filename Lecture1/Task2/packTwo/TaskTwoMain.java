0cc6cc4213b367dee7e299d07a64c5bc03f1c37b
carmenhorendum <carmenhorendum@gmail.com>
2018-11-10 22:45:26 +0300

    Update TaskTwoMain.java

--- Lecture1/Task2/packTwo/TaskTwoMain.java
+++ Lecture1/Task2/packTwo/TaskTwoMain.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 
 public class TaskTwoMain {
 	public static void main(String[] args) {
-		String dayNames[] = new DateFormatSymbols().getWeekdays();
-		System.out.println("Enter weekday number (1-7):");
-		Scanner input = new Scanner(System.in);
-		int dayNumber = Integer.parseInt(input.next());
-		System.out.println(String.format("Your day number is %d - %s.", dayNumber, dayNames[dayNumber]));
+		getDayNameFromNumber();
 	}
+	
+	private static void getDayNameFromNumber() {
+		String dayNames[] = new DateFormatSymbols().getWeekdays();
+		int dayNumber;
+		do {
+			System.out.println("Enter weekday number (1-7):");
+			Scanner input = new Scanner(System.in);
+			dayNumber = input.nextInt();
+		} while (dayNumber <= 0 || dayNumber > 7);
+		System.out.println(String.format("Your day number is %d - %s.", dayNumber, dayNames[dayNumber]));
+	}	
 }
