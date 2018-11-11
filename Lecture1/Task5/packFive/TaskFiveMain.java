4c94f408dd9550672f37d57ba2eb1f1e9ffaf2c0
carmenhorendum <carmenhorendum@gmail.com>
2018-11-11 12:41:04 +0300

    Update TaskFiveMain.java

--- Lecture1/Task5/packFive/TaskFiveMain.java
+++ Lecture1/Task5/packFive/TaskFiveMain.java
@@ -13,53 +13,59 @@ public class TaskFiveMain {
 		Set picked = new HashSet();
 		while (!mask.equals(answer)) {
 			System.out.println();
-			System.out.println(String.format("Make your guess: %s. You can enter a letter or a word.",mask));
+			System.out.println(String.format("\nMake your guess: %s. You can enter a letter or a word.", mask));
 			Scanner input = new Scanner(System.in);
 			String guess = input.nextLine().toUpperCase();
 			int caseId = 0;
 			if (picked.contains(guess)) {
-				caseId = 3;
+				getRepeated(picked, guess);
 			} else {
-				if (guess.length()==1) {
+				if (guess.length() == 1) {
+					boolean flag = false;
 					for (int i = 0; i < answerArray.length; i++) {
-						if (answerArray[i]==guess.charAt(0)) {
-							caseId = 1;
-							guessed.replace(i,i+1,answer.substring(i,i+1));
+						if (answerArray[i] == guess.charAt(0)) {
+							guessed.replace(i, i + 1, answer.substring(i, i + 1));
 							mask = guessed.toString();
-						} else if (mask.equals(answer)) {
-							caseId = 2;
+							flag = true;
 						}
+					}
+					if (flag == false) {
+						getBadGuess(picked, guess, guessed);
+					} else {
+						getGoodGuess(picked, guess, guessed);
 					}
 				} else {
 					if (answer.toUpperCase().contains(guess)) {
-						caseId = 1;
 						int startIndex = answer.toUpperCase().indexOf(guess);
-						guessed.replace(startIndex,startIndex+guess.length(),
-							answer.substring(startIndex,startIndex+guess.length()));
+						guessed.replace(startIndex,startIndex + guess.length(),
+								answer.substring(startIndex, startIndex + guess.length()));
 						mask = guessed.toString();
-					} else if (mask.equals(answer)) {
-						caseId = 2;
+						getGoodGuess(picked, guess, guessed);
+					} else {
+						getBadGuess(picked, guess, guessed);
 					}
 				}
 			}
-			switch (caseId) {
-				case 1:
-					picked.add(guess);
-					System.out.println(String.format("Good guess: %s belongs to %s!",guess,guessed));
-					break;
-				case 2:
-					System.out.println();
-					System.out.println(String.format("You win! Correct answer is: %s",answer));
-					break;
-				case 3:
-					picked.add(guess);
-					System.out.println(String.format("You've already picked letter %s! Try something different.",guess));
-					break;
-				default:
-					picked.add(guess);
-					System.out.println(String.format("Bad guess: %s doesn't belong to %s!",guess,guessed));
-					break;
-			}
 		}
+		getWin(answer);
+	}
+
+	private static void getBadGuess(Set picked, String guess, StringBuffer guessed) {
+		picked.add(guess);
+		System.out.println(String.format("Bad guess: %s doesn't belong to %s!", guess, guessed));
+	}
+
+	private static void getGoodGuess(Set picked, String guess, StringBuffer guessed) {
+		picked.add(guess);
+		System.out.println(String.format("Good guess: %s belongs to %s!", guess, guessed));
+	}
+
+	private static void getRepeated(Set picked, String guess) {
+		picked.add(guess);
+		System.out.println(String.format("You've already picked %s! Try something different.", guess));
+	}
+
+	private static void getWin(String answer) {
+		System.out.println(String.format("\nYou win! Correct answer is: %s", answer));
 	}
 }
