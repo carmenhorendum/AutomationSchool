d8c7ef72f1873963607e8ad98e1a6a5327e99845
carmenhorendum <carmenhorendum@gmail.com>
2018-11-13 23:38:32 +0300

    Update TaskFourMain.java

--- Lecture2/Task2/TaskTwoMain.java
+++ Lecture2/Task2/TaskTwoMain.java
@@ -5,12 +5,12 @@ public class TaskTwoMain {
 		final String FIRST_LETTER = "M";
 		List<Student> students = new ArrayList<>();
 
-		students.add(new Student(1, 60,"Rudolf","Schenker"));
-		students.add(new Student(2, 75,"Klaus","Meine"));
-		students.add(new Student(3,55,"Matthias","Jabs"));
-		students.add(new Student(4,58,"Paweł","Mąciwoda"));
-		students.add(new Student(5,64,"Mikkey","Dee"));
-		
+		students.add(new Student(1, 60, "Rudolf", "Schenker"));
+		students.add(new Student(2, 75, "Klaus", "Meine"));
+		students.add(new Student(3, 55, "Matthias", "Jabs"));
+		students.add(new Student(4, 58, "Paweł", "Mąciwoda"));
+		students.add(new Student(5, 64, "Mikkey", "Dee"));
+
 		Collections.sort(students);
 		System.out.println("List sorted by Age:");
 		for (Student student : students) {
@@ -24,17 +24,18 @@ public class TaskTwoMain {
 		}
 
 		double average = 0;
-		System.out.println(String.format("Average age of students is:\n%s", getAvarageAge(average, students)));
+		System.out.println(String.format("Average age of students is:\n%s",
+				getAvarageAge(average, students)));
 
 		System.out.println("Names from the Map:");
 		Map<Integer, Student> studentsMap = getStudentsListToMap(students);
-		for (Student key: studentsMap.values()) {
+		for (Student key : studentsMap.values()) {
 			System.out.println(key.getName());
 		}
 
 		System.out.println("Names from the Map (id > 3):");
 		Map<Integer, Student> studentMapAboveThree = new HashMap<>();
-		for (Student key: studentsMap.values()) {
+		for (Student key : studentsMap.values()) {
 			if (key.getId() > 3) {
 				studentMapAboveThree.put(key.getId(), key);
 				System.out.println(key.getName());
@@ -51,9 +52,9 @@ public class TaskTwoMain {
 		}
 		return studentsM;
 	}
-	
+
 	private static double getAvarageAge(double average, List<Student> students) {
-		if (students.size() > 0)	{
+		if (students.size() > 0) {
 			double sum = 0;
 			for (Student student : students) {
 				sum += student.getAge();
--- Lecture2/Task3/TaskThreeMain.java
+++ Lecture2/Task3/TaskThreeMain.java
@@ -6,38 +6,54 @@ import java.util.stream.Collectors;
 
 public class TaskThreeMain {
 	public static void main(String[] args) {
+		final String FIRST_LETTER = "M";
 		List<Student> students = new ArrayList<>();
 
-		students.add(new Student(1, 60,"Rudolf","Schenker"));
-		students.add(new Student(2, 75,"Klaus","Meine"));
-		students.add(new Student(3,55,"Matthias","Jabs"));
-		students.add(new Student(4,58,"Paweł","Mąciwoda"));
-		students.add(new Student(5,64,"Mikkey","Dee"));
+		students.add(new Student(1, 60, "Rudolf", "Schenker"));
+		students.add(new Student(2, 75, "Klaus", "Meine"));
+		students.add(new Student(3, 55, "Matthias", "Jabs"));
+		students.add(new Student(4, 58, "Paweł", "Mąciwoda"));
+		students.add(new Student(5, 64, "Mikkey", "Dee"));
 
-		students = getSortByAge(students);
 		System.out.println("List sorted by Age:");
+		students = getSortByAge(students);
 		for (Student student : students) {
 			System.out.println(student.getAge() + " " + student.getName());
 		}
 
+		System.out.println(String.format("First letter of the name is %s:", FIRST_LETTER));
+		List<Student> studentsM = getStudentsM(students, FIRST_LETTER);
+		for (Student student : studentsM) {
+			System.out.println(student.getName());
+		}
+
+		System.out.println(String.format("Average age of students is:\n%s",
+				getAverageAge(students)));
+
 		System.out.println("Names from the Map:");
 		Map<Integer, Student> studentsMap = getStudentsListToMap(students);
-		for (Student key: studentsMap.values()) {
-			System.out.println(key.getName());
+		for (Student key : studentsMap.values()) {
+			System.out.println(key.getId() + " " + key.getName());
 		}
 
-		System.out.println("Names from the Map (id > 3):");
 		studentsMap = getMapAboveThree(students);
-		for (Student key: studentsMap.values()) {
-			System.out.println(key.getName());
-		}
-
+		System.out.println("Names from the Map (id > 3):");
+		studentsMap.forEach((integer, student) ->
+				System.out.println(integer + " " + student.getName()));
 	}
 
 	private static List<Student> getSortByAge(List<Student> students) {
 		Comparator<Student> byAge = Comparator.comparingInt(Student::getAge);
-		students = students.stream().sorted(byAge).collect(Collectors.toList());
-		return students;
+		return students.stream().sorted(byAge).collect(Collectors.toList());
+	}
+
+	private static List<Student> getStudentsM(List<Student> students, String FIRST_LETTER) {
+		return students.stream().filter(name -> name.getName().startsWith(FIRST_LETTER))
+				.collect(Collectors.toList());
+	}
+
+	private static double getAverageAge(List<Student> students) {
+		return students.stream().mapToInt(Student::getAge).average().getAsDouble();
 	}
 
 	private static Map<Integer, Student> getStudentsListToMap(List<Student> students) {
@@ -45,8 +61,7 @@ public class TaskThreeMain {
 	}
 
 	private static Map<Integer, Student> getMapAboveThree(List<Student> students) {
-		return students.entrySet().stream()
-				.filter(map -> map.getKey() > 3)
-				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		return students.stream().filter((id) -> id.getId() > 3)
+				.collect(Collectors.toMap(Student::getId, student -> student));
 	}
 }
