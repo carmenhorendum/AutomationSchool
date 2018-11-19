96c1a1e8ef6f2b2c067d7e043d0b236cc32b51df
carmenhorendum <carmenhorendum@gmail.com>
2018-11-20 01:02:05 +0300

    Task 1 Object Model Reworked

--- Lecture2/Task1/client/TaskOneMain.java
+++ Lecture2/Task1/client/TaskOneMain.java
@@ -1,43 +1,21 @@
 package client;
 
+import product.Filter;
 import product.Product;
 import product.StockManager;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TaskOneMain {
 	public static void main(String[] args) {
 		List<Product> products = new ArrayList<>();
-		List<Product> newStock = StockManager.getGuitarsToStock(products);
+		List<Product> newStock = StockManager.getProductsToStock(products);
 		System.out.println(newStock);
-		System.out.println(getStockPrice(newStock));
-		System.out.println(getSortByPrice(newStock));
-		System.out.println(getSortByStringsCount(newStock));
-		System.out.println(getProductByParameters(newStock));
-	}
-
-	private static double getStockPrice(List<Product> products) {
-		return products.stream().mapToDouble(Product::getPrice).sum();
-	}
-
-	private static List<Product> getSortByPrice(List<Product> products) {
-		Comparator<Product> byPrice = Comparator.comparing(Product::getPrice);
-		return products.stream().sorted(byPrice).collect(Collectors.toList());
-	}
-
-	private static List<Product> getSortByStringsCount(List<Product> products) {
-		Comparator<Product> byStringsCount
-				= Comparator.comparingInt((Product product) -> product.getGuitar().getStringsCount());
-		return products.stream().sorted(byStringsCount).collect(Collectors.toList());
-	}
-
-	private static List<Product> getProductByParameters(List<Product> products) {
-		return products.stream()
-				.filter(product -> (product.getPrice() < 2000))
-				.filter(product -> (product.getWarranty() > 12))
-				.collect(Collectors.toList());
+		System.out.println(newStock.size());
+		System.out.println(Filter.getStockPrice(newStock));
+		System.out.println(Filter.getSortByPrice(newStock));
+		System.out.println(Filter.getSortByStringsCount(newStock));
+		System.out.println(Filter.getProductByParameters(newStock));
 	}
 }
