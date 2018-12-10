d93f8397433afba3996b8b08bf1d1a8bb4bbfc19
carmenhorendum <carmenhorendum@gmail.com>
2018-12-10 12:54:35 +0300

    Update CalcUnitTests.java

--- Lecture4/src/test/java/CalcUnitTests.java
+++ Lecture4/src/test/java/CalcUnitTests.java
@@ -4,36 +4,18 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CalcUnitTests {
-	private double op1;
-	private double op2;
-	private Operator operator;
-
-	private Calculator newCalc() {
-		return new Calculator(op1, operator, op2);
-	}
-
-	private double getOperand(double operand) {
-		return operand;
-	}
-
-	private Operator getOperator(Operator operator) {
-		return operator;
-	}
 
 	@BeforeClass
 	public void oneTimeSetUp() {
-		newCalc();
-		System.out.print("Before Class: newCalc();");
+		System.out.print("Before Class");
 	}
 
-	@Test(dataProvider = "testAddDataProvider")
-	public void add_twoNumbers_returnsSum(double operand1, double operand2, double total) {
-		operator = Operator.PLUS;
-		op1 = getOperand(operand1);
-		op2 = getOperand(operand2);
-		System.out.println("Test Total: " + newCalc().makeCalculation());
-		Assert.assertEquals(newCalc().makeCalculation(), total);
-	}
+        @Test(dataProvider = "testAddDataProvider")
+        public void checkSum(double operand1, double operand2, double total) {
+            Calculator calculator = new Calculator(operand1, Operator.PLUS, operand2);
+            System.out.println("Test Total: " + calculator.makeCalculation());
+            Assert.assertEquals(calculator.makeCalculation(), total);
+        }
 
 	@DataProvider(name = "testAddDataProvider")
 	public Object[][] parameterAddTestProvider() {
@@ -48,14 +30,12 @@ public class CalcUnitTests {
 		};
 	}
 
-	@Test(dataProvider = "testSubDataProvider")
-	public void sub_twoNumbers_returnsDif(double operand1, double operand2, double total) {
-		operator = Operator.MINUS;
-		op1 = getOperand(operand1);
-		op2 = getOperand(operand2);
-		System.out.println("Test Total: " + newCalc().makeCalculation());
-		Assert.assertEquals(newCalc().makeCalculation(), total);
-	}
+        @Test(dataProvider = "testSubDataProvider")
+        public void checkDifference(double operand1, double operand2, double total) {
+            Calculator calculator = new Calculator(operand1, Operator.MINUS, operand2);
+            System.out.println("Test Total: " + calculator.makeCalculation());
+            Assert.assertEquals(calculator.makeCalculation(), total);
+        }
 
 	@DataProvider(name = "testSubDataProvider")
 	public Object[][] parameterSubTestProvider() {
@@ -70,14 +50,12 @@ public class CalcUnitTests {
 		};
 	}
 
-	@Test(dataProvider = "testMultDataProvider")
-	public void mult_twoNumbers_returnsProd(double operand1, double operand2, double total) {
-		operator = Operator.MULTIPLY;
-		op1 = getOperand(operand1);
-		op2 = getOperand(operand2);
-		System.out.println("Test Total: " + newCalc().makeCalculation());
-		Assert.assertEquals(newCalc().makeCalculation(), total);
-	}
+        @Test(dataProvider = "testMultDataProvider")
+        public void checkProduct(double operand1, double operand2, double total) {
+            Calculator calculator = new Calculator(operand1, Operator.MULTIPLY, operand2);
+            System.out.println("Test Total: " + calculator.makeCalculation());
+            Assert.assertEquals(calculator.makeCalculation(), total);
+        }
 
 	@DataProvider(name = "testMultDataProvider")
 	public Object[][] parameterMultTestProvider() {
@@ -91,17 +69,14 @@ public class CalcUnitTests {
 		};
 	}
 
-	@Test(expectedExceptions = IllegalArgumentException.class,
-			expectedExceptionsMessageRegExp = "Error: Can't divide by zero!",
-			dataProvider = "testDivByZeroDataProvider")
-	public void div_byZero_returnsIllegalArgumentException(double operand1, double operand2, double total)
-			throws IllegalArgumentException {
-		operator = Operator.DIVIDE;
-		op1 = getOperand(operand1);
-		op2 = getOperand(operand2);
-		System.out.println("New IllegalArgumentException was thrown");
-		Assert.assertEquals(newCalc().makeCalculation(), total);
-	}
+        @Test(expectedExceptions = IllegalArgumentException.class,
+                expectedExceptionsMessageRegExp = "Error: Can't divide by zero!",
+                dataProvider = "testDivByZeroDataProvider")
+        public void checkDivisionByZero(double operand1, double operand2, double total)
+                throws IllegalArgumentException {
+            Calculator calculator = new Calculator(operand1, Operator.DIVIDE, operand2);
+            Assert.assertEquals(calculator.makeCalculation(), total);
+        }
 
 	@DataProvider(name = "testDivByZeroDataProvider")
 	public Object[][] parametersDivByZeroTestProvider() {
@@ -112,15 +87,13 @@ public class CalcUnitTests {
 		};
 	}
 
-	@Test(dependsOnMethods = "div_byZero_returnsIllegalArgumentException",
-			alwaysRun = true, dataProvider = "testDivDataProvider")
-	public void div_twoNumbers_returnsQuot(double operand1, double operand2, double total) {
-		operator = Operator.DIVIDE;
-		op1 = getOperand(operand1);
-		op2 = getOperand(operand2);
-		System.out.println("Test Total: " + newCalc().makeCalculation());
-		Assert.assertEquals(newCalc().makeCalculation(), total);
-	}
+        @Test(dependsOnMethods = "checkDivisionByZero",
+                alwaysRun = true, dataProvider = "testDivDataProvider")
+        public void checkQuotient(double operand1, double operand2, double total) {
+            Calculator calculator = new Calculator(operand1, Operator.DIVIDE, operand2);
+            System.out.println("Test Total: " + calculator.makeCalculation());
+            Assert.assertEquals(calculator.makeCalculation(), total);
+        }
 
 	@DataProvider(name = "testDivDataProvider")
 	public Object[][] parameterDivTestProvider() {
