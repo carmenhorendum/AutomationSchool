663527c8701da18dbcf4ce6d4e9115de01b7ab3f
carmenhorendum <carmenhorendum@gmail.com>
2018-12-27 09:13:36 +0300

    Lecture 6 Home Task Implementation

--- Lecture6/src/test/java/BaseTest.java
+++ Lecture6/src/test/java/BaseTest.java
@@ -1,8 +1,4 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.server.SeleniumServer;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
@@ -12,31 +8,8 @@ public class BaseTest {
 //		String seleniumPort = context.getCurrentXmlTest().getParameter("selenium.port");
 //		String seleniumBrowser = context.getCurrentXmlTest().getParameter("selenium.browser");
 //		String seleniumUrl = context.getCurrentXmlTest().getParameter("selenium.url");
-
-//		RemoteControlConfiguration rcc = new RemoteControlConfiguration();
-//		rcc.setSingleWindow(true);
-//		rcc.setPort(Integer.parseInt(seleniumPort));
-
-//		try {
-//			server = new SeleniumServer(false, rcc);
-//			server.boot();
-//		} catch (Exception e) {
-//			throw new IllegalStateException("Can't start selenium server", e);
-//		}
-
-//		proc = new HttpCommandProcessor(seleniumHost, Integer.parseInt(seleniumPort),
-//				seleniumBrowser, seleniumUrl);
-
-//		selenium = new DefaultSelenium(proc);
-//		selenium.start();
 		String path = System.getProperty("user.dir");
 		System.out.println(path);
 		System.setProperty("webdriver.chrome.driver", path + "/src/test/resources/chromedriver.exe");
 	}
-
-//	@AfterSuite(alwaysRun = true)
-//	public void setupAfterSuite() {
-//		selenium.stop();
-//		server.stop();
-//	}
 }
